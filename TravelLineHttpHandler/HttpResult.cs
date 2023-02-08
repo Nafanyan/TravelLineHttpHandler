0386d93345dd557b68ec1ec809bebae7e1f587e6
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-02-08 14:22:54 +0300

    Create class HttpResult

---
+++ TravelLineHttpHandler/HttpResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelLineHttpHandler
+{
+    class HttpResult
+    {
+        public string Url { get; set; }
+        public string RequestBody { get; set; }
+        public string ResponseBody { get; set; }
+    }
+}
